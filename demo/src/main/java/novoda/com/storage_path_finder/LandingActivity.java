3392083ad1f3d2c53403866e1be613b1cc9773c7
Ryan Feline <ryan.feline@novoda.com>
2018-06-18 15:30:19 +0100

    Add beginning of the demo app.

---
+++ demo/src/main/java/novoda/com/storage_path_finder/LandingActivity.java
@@ -0,0 +1,13 @@
+package novoda.com.storage_path_finder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LandingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+    }
+}
