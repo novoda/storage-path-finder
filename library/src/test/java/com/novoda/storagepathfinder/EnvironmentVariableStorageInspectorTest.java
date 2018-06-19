741f4f206e0aa8a534839b85c02989d627dd47ff
Ryan Feline <ryan.feline@novoda.com>
2018-06-19 12:50:37 +0100

    Add environment variable storage inspector test to appease analysis checks.

---
+++ library/src/test/java/com/novoda/storagepathfinder/EnvironmentVariableStorageInspectorTest.java
@@ -0,0 +1,44 @@
+package com.novoda.storagepathfinder;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Test;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+public class EnvironmentVariableStorageInspectorTest {
+
+    private static final String SECONDARY_STORAGE_PATH = "storage/MicroSD";
+    private static final String NO_SECONDARY_STORAGE_PATH = null;
+
+    @Test
+    public void returnsSecondaryDeviceStorageRoots() {
+        EnvironmentVariableStorageInspector storageInspector = createEnvironmentStorageInspectorWithPath(SECONDARY_STORAGE_PATH);
+
+        List<DeviceStorageRoot> secondaryDeviceStorageRoots = storageInspector.getSecondaryDeviceStorageRoots();
+
+        List<DeviceStorageRoot> expectedRoots = Collections.singletonList(
+                new DeviceStorageRoot(SECONDARY_STORAGE_PATH, SECONDARY_STORAGE_PATH, DeviceStorageRoot.Type.SECONDARY)
+        );
+        assertThat(secondaryDeviceStorageRoots).isEqualTo(expectedRoots);
+    }
+
+    @Test
+    public void returnsNoSecondaryDeviceStorageRoots() {
+        EnvironmentVariableStorageInspector storageInspector = createEnvironmentStorageInspectorWithPath(NO_SECONDARY_STORAGE_PATH);
+
+        List<DeviceStorageRoot> secondaryDeviceStorageRoots = storageInspector.getSecondaryDeviceStorageRoots();
+
+        assertThat(secondaryDeviceStorageRoots).isEmpty();
+    }
+
+    private static EnvironmentVariableStorageInspector createEnvironmentStorageInspectorWithPath(String storagePath) {
+        AndroidSystem androidSystem = mock(AndroidSystem.class);
+        given(androidSystem.getEnv(anyString())).willReturn(storagePath);
+        return new EnvironmentVariableStorageInspector(androidSystem);
+    }
+}
