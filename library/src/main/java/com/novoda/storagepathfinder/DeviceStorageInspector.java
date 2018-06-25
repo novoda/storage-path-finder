7e089464edab7a0981f03cde37542c846be0b8dc
Neil <neil@novoda.com>
2018-06-25 15:27:43 +0100

    add full stops

--- library/src/main/java/com/novoda/storagepathfinder/DeviceStorageInspector.java
+++ library/src/main/java/com/novoda/storagepathfinder/DeviceStorageInspector.java
@@ -5,25 +5,25 @@ import java.util.List;
 public interface DeviceStorageInspector {
 
     /**
-     * The system storage path of your primary storage
+     * The system storage path of your primary storage.
      * (this is most likely built in phone storage)
      */
     StoragePath getPrimaryStorageBasePath();
 
     /**
-     * The system storage path of your secondary storage
+     * The system storage path of your secondary storage.
      * (this is most likely your SD Card)
      */
     List<StoragePath> getSecondaryStorageBasePath();
 
     /**
-     * The application storage path of your primary storage
+     * The application storage path of your primary storage.
      * (this is most likely built in phone storage)
      */
     StoragePath getPrimaryStorageApplicationPath();
 
     /**
-     * The application storage path of your secondary storage
+     * The application storage path of your secondary storage.
      * (this is most likely your SD Card)
      */
     List<StoragePath> getSecondaryStorageApplicationPath();
