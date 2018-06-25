fc0413714080781d8c396550ee60217c4a199c08
Neil <neil@novoda.com>
2018-06-25 11:22:08 +0100

    fix compilation error

--- demo/src/main/java/com/novoda/storagepathfinder/demo/LandingActivity.java
+++ demo/src/main/java/com/novoda/storagepathfinder/demo/LandingActivity.java
@@ -38,7 +38,7 @@ public class LandingActivity extends AppCompatActivity {
     }
 
     private final StoragePathViewHolder.Listener onAddFileClicked = deviceStoragePath -> {
-        String pathToCloneTo = deviceStoragePath.getApplicationPath() + "/" + ASSET_NAME;
+        String pathToCloneTo = deviceStoragePath.asString() + "/" + ASSET_NAME;
         assetCloner.cloneAsset(ASSET_NAME, pathToCloneTo);
     };
 
