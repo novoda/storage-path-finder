2f4e42cf3f98756441af29f901813a5389b327b9
Ryan Feline <ryan.feline@novoda.com>
2018-06-27 22:24:16 +0100

    Convert AssetCloner to Kotlin.

---
+++ demo/src/main/java/com/novoda/storagepathfinder/demo/AssetCloner.kt
@@ -0,0 +1,26 @@
+package com.novoda.storagepathfinder.demo
+
+import android.content.res.AssetManager
+import android.util.Log
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.util.concurrent.Executor
+
+internal class AssetCloner(private val assetManager: AssetManager, private val executor: Executor) {
+
+    fun cloneAsset(assetName: String, pathToCloneTo: String) {
+        executor.execute {
+            File(pathToCloneTo).also {
+                it.parentFile?.takeIf { !it.exists() }?.also { it.mkdirs() }
+            }.also { copyAssetToFile(assetName, it) }
+        }
+    }
+
+    private fun copyAssetToFile(assetName: String, outputFile: File) = try {
+        assetManager.open(assetName)
+                .copyTo(FileOutputStream(outputFile, true))
+    } catch (e: IOException) {
+        Log.e(javaClass.simpleName, "Failed to close streams.", e)
+    }
+}
