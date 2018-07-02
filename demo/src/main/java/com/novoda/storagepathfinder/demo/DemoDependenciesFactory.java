cb8c5e7cee49bccb4d5df87ab95875904c365dc0
Daniele Bonaldo <dany.bony@gmail.com>
2018-07-02 16:09:27 +0200

    Use builder in demo app instead of manually creating all the dependencies

--- demo/src/main/java/com/novoda/storagepathfinder/demo/DemoDependenciesFactory.java
+++ demo/src/main/java/com/novoda/storagepathfinder/demo/DemoDependenciesFactory.java
@@ -3,14 +3,7 @@ package com.novoda.storagepathfinder.demo;
 import android.content.Context;
 import android.content.res.AssetManager;
 
-import com.novoda.storagepathfinder.AndroidDeviceFeatures;
 import com.novoda.storagepathfinder.AndroidDeviceStorageInspector;
-import com.novoda.storagepathfinder.AndroidExternalStorageDirectories;
-import com.novoda.storagepathfinder.AndroidFileSystem;
-import com.novoda.storagepathfinder.AndroidSystem;
-import com.novoda.storagepathfinder.DeviceFeatures;
-import com.novoda.storagepathfinder.ExternalStorageDirectories;
-import com.novoda.storagepathfinder.FileSystem;
 
 import java.util.concurrent.Executors;
 
@@ -26,15 +19,6 @@ public final class DemoDependenciesFactory {
     }
 
     public static AndroidDeviceStorageInspector createStorageInspector(Context context) {
-        ExternalStorageDirectories externalStorageDirectories = new AndroidExternalStorageDirectories(context.getApplicationContext());
-        FileSystem fileSystem = new AndroidFileSystem(externalStorageDirectories);
-        DeviceFeatures deviceFeatures = new AndroidDeviceFeatures();
-        AndroidSystem androidSystem = new AndroidSystem();
-        return new AndroidDeviceStorageInspector(
-                context.getApplicationContext(),
-                fileSystem,
-                deviceFeatures,
-                androidSystem
-        );
+        return AndroidDeviceStorageInspector.builder(context).build();
     }
 }
